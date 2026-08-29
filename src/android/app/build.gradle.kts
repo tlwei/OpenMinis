4bdb6072acf608d2c99ca1c11e469e19c5a9729d
tlwei <mycc112@qq.com>
2026-08-29 11:27:06 +0800

    Use isolated application ID for co-installable B mode build

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.openminis.app"
+        applicationId = "com.openminis.app.rawaudio"
         minSdk = 26
         targetSdk = 35
         versionCode = 24
